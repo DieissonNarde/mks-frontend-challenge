5428de24258503664577bd279d925075abfd0f45
DieissonNarde <dieissonnarde@gmail.com>
2023-01-29 19:17:37 -0400

    feat: initial setup

---
+++ src/styles/global.ts
@@ -0,0 +1,24 @@
+import { createGlobalStyle } from 'styled-components'
+
+export default createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  :root {
+    font-size: 62.5%; /* 1rem = 10px */
+    font-family: 'Roboto', sans-serif;
+  }
+
+  body {
+    background: ${props => props.theme.colors.background};
+    color: ${props => props.theme.colors.text};
+  }
+
+  a {
+    color: ${props => props.theme.colors.background};
+    text-decoration: none;
+  }
+`
