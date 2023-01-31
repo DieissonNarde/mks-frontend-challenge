e4e7c9c94ef1af6eac89644b684d69e6ba5c4a20
DieissonNarde <dieissonnarde@gmail.com>
2023-01-30 23:15:34 -0400

    feat: add footer component

---
+++ src/components/Footer.tsx
@@ -0,0 +1,9 @@
+import { FooterContent } from '../styles/components/footer'
+
+export function Footer() {
+  return (
+    <FooterContent>
+      <span>MKS Sistemas © Todos os direitos reservados</span>
+    </FooterContent>
+  )
+}
---
+++ src/styles/components/footer.ts
@@ -0,0 +1,14 @@
+import styled from 'styled-components'
+
+export const FooterContent = styled.div`
+  background: #eeeeee;
+  width: 100%;
+  padding-block: 0.9rem;
+
+  text-align: center;
+  font-weight: 400;
+  font-size: 1.2rem;
+  line-height: 1.5rem;
+
+  color: #000000;
+`
