53761f689fef9ddea5ebbd270da59354a8daa5f6
DieissonNarde <dieissonnarde@gmail.com>
2023-02-02 15:33:40 -0400

    feat(CartButton): implement click to open cart

--- src/components/CartButton.tsx
+++ src/components/CartButton.tsx
@@ -2,9 +2,25 @@ import { FiShoppingCart } from 'react-icons/fi'
 
 import { CartButtonContent } from '../styles/components/cartButton'
 
-export default function CartButton() {
+interface CartButtonProps {
+  open: boolean
+  setOpen: (open: boolean) => void
+}
+
+export default function CartButton({
+  open,
+  setOpen,
+  ...props
+}: CartButtonProps) {
+  const isExpanded = open ? true : false
+
   return (
-    <CartButtonContent>
+    <CartButtonContent
+      aria-label="Toggle menu"
+      aria-expanded={isExpanded}
+      onClick={() => setOpen(!open)}
+      {...props}
+    >
       <div>
         <FiShoppingCart />
       </div>
